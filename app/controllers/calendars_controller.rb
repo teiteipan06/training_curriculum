06a01b9f7177e15ecd9ac8b31e771e677a1c8cbd
teiteipan06 <y7321543@gmail.com>
2022-09-12 19:44:17 +0900

    issue4-6修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,6 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    #binding.pry
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -38,9 +37,9 @@ class CalendarsController < ApplicationController
       end
       #wday_num = Date.today.wday # wdayメソッドを用いて取得した数値
       wday_num += 1
-      #if wday_num >= 7 #「wday_numが7以上の場合」という条件式
-        #wday_num = wday_num -7
-      #end
+      if wday_num >= 7 #「wday_numが7以上の場合」という条件式
+        wday_num = wday_num -7
+      end
       days = { :month => (@todays_date + x).month, :date => (@todays_date + x).day, :plans => today_plans, :wday => wdays[wday_num]}
       @week_days.push(days)
     end
